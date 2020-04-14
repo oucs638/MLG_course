17a523fcde5ff89e34612300a999da405c311016
oucs638 <f74062028@gs.ncku.edu.tw>
2020-04-15 00:45:19 +0800

    update

--- ml_play.py
+++ ml_play.py
@@ -27,7 +27,7 @@ def ml_loop():
     # 1. Put the initialization code here.
     ball_served = False
     filename = path.join(path.dirname(__file__), 'save',
-                         'clf_KNN_BallAndDirection.pickle')
+                         'clf_RANF_BallAndDirection.pickle')
     with open(filename, 'rb') as file:
         clf = pickle.load(file)
     bal_lst = [93, 395]
